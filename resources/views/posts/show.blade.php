a662734a06b0298b89b3617a48b91b0b68b27d90
ivan3123708 <ivan3123708@gmail.com>
2018-09-29 11:29:56 +0200

    Edit & delete data

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,4 +7,10 @@
   <div>
     {!! $post->body !!}
   </div>
+  <hr>
+  <a href="/lsapp/public/posts/{{ $post->id }}/edit" class="btn btn-default">Edit</a>
+  {!! Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right']) !!}
+    {{ Form::hidden('_method', 'DELETE') }}
+    {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+  {!! Form::close() !!}
 @endsection
